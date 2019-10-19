e229026e7ec247a4c57386dea67f507836d460c4
Newton Munene <newtonmunene99@gmail.com>
2019-10-19 10:42:56 +0300

    Added firebase config to Production environment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,24 @@ import { HomeComponent } from "./pages/home/home.component";
 import { MovieComponent } from "./pages/movie/movie.component";
 import { FavouritesComponent } from "./pages/favourites/favourites.component";
 import { SearchComponent } from "./pages/search/search.component";
+import {
+  redirectUnauthorizedTo,
+  AngularFireAuthGuard
+} from "@angular/fire/auth-guard";
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo([""]);
 const routes: Routes = [
   { path: "", component: HomeComponent },
   {
     path: "movie/:id",
     component: MovieComponent
   },
-  { path: "favourites", component: FavouritesComponent },
+  {
+    path: "favourites",
+    component: FavouritesComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
+  },
   { path: "search", component: SearchComponent }
 ];
 
